3f39f2c1e99f9d335f2e844b7948406782296c6f
jipengfei <jipengfei@pc-77-146.customer.ask4.lan>
2023-11-19 19:53:06 +0000

    modify gitignore file.

---
+++ SwiftExample/Variables.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let myName = "Pengfei Ji"
+let yourName = "Congrui Wang"
+
+let names = [
+    myName,
+    yourName
+]
+names.append("bar")
+names.append("beer")
+print(names)
+
+
